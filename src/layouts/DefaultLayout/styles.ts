53ee30dd43bf664d0dab13454b76c7ad9f92b65d
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-12-28 22:24:56 -0300

    feat

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const LayoutContainer = styled.div`
   max-width: 100vw;
   height: 100vh;
+
+  
   /* margin: 5rem auto;
   max-width: 74rem;
   height: calc(100vh - 10rem);
